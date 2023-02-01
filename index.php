36d637a2f4dc7fe28f450d7a7b4987f6df358dd5
eli <elzr.sr@gmail.com>
2023-02-01 23:32:54 +0100

    basic start

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <!-- Script temporal -->
+    <script>
+      function refreshPage() {
+        location.reload()
+      }
+      // setInterval(refreshPage, 2000) // 5000 ms
+    </script>
+    <!-- Fin script -->
+  </head>
+  <body>
+    <h1>Links</h1>
+    <a href="check.php">Consultar datos</a>
+    <br>
+    <a href="login.php">Login</a>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <h1>Iniciar sesión</h1>
+    <div>
+        <span>Usuario</span>
+        <input type="text">
+    </div>
+    <div>
+        <span>Contraseña</span>
+        <input type="text">
+    </div>
+    <button>Continuar</button>
+    
+</body>
+</html>
