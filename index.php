5dc677b38d390ebc3af2f9d0254f27c07a7218a2
eli <elzr.sr@gmail.com>
2023-04-05 13:35:27 +0200

    Index cambiante según sesión muy básico

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
+<?php
+session_start();
+if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['user'])) {
+  $user = $_SESSION['user'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Home</title>
-    <!-- Script temporal -->
-    <script>
-      function refreshPage() {
-        location.reload()
-      }
-      // setInterval(refreshPage, 2000) // 5000 ms
-    </script>
-    <!-- Fin script -->
-  </head>
-  <body>
-    <h1>Links</h1>
-    <a href="check/">Consultar datos</a>
-    <br>
-    <a href="login.php">Login</a>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Home</title>
+</head>
+
+<body>
+  <h1>Links</h1>
+  <a href="check/">Consultar datos</a>
+  <br>
+  <?php
+  if (isset($user)) {
+    echo "Bienvenido " . $user . "!";
+  } else {
+    echo "<a href='login.php'>Login</a>";
+  }
+  ?>
+</body>
+
 </html>
