0046a27dc92c111319635aecec973a2380707595
eli <elzr.sr@gmail.com>
2023-04-22 20:28:56 +0200

    Añadidos asides estilados basicamente y con funciones basicas

--- js/mapaPanel.js
+++ js/mapaPanel.js
@@ -2,8 +2,24 @@ const panelLinea = document.getElementById('panelLinea')
 const panelParadas = document.getElementById('panelParadas')
 const panelComoLlegar = document.getElementById('panelComoLlegar')
 const biciLegend = document.getElementsByClassName('bici-legend')[0]
+const aside2 = document.getElementsByClassName('aside-2')[0]
+
+let menuOpen = false
+
+function openMenu() {
+  menuOpen = true
+  aside2.style.animation = 'openMenu 500ms'
+  aside2.style.left = '0'
+}
+
+function closeMenu() {
+  menuOpen = false
+  aside2.style.animation = 'closeMenu 500ms forwards'
+  aside2.style.left = '-300px'
+}
 
 function showLineas() {
+  if (!menuOpen) openMenu()
   panelLinea.style.display = 'flex'
   panelParadas.style.display = 'none'
   panelComoLlegar.style.display = 'none'
@@ -12,6 +28,7 @@ function showLineas() {
 }
 
 function showParadas() {
+  if (!menuOpen) openMenu()
   panelLinea.style.display = 'none'
   panelParadas.style.display = 'flex'
   panelComoLlegar.style.display = 'none'
@@ -20,6 +37,7 @@ function showParadas() {
 }
 
 function showBicis() {
+  if (!menuOpen) openMenu()
   panelLinea.style.display = 'none'
   panelParadas.style.display = 'none'
   panelComoLlegar.style.display = 'none'
@@ -29,6 +47,7 @@ function showBicis() {
 }
 
 function showComoLlegar() {
+  if (!menuOpen) openMenu()
   panelLinea.style.display = 'none'
   panelParadas.style.display = 'none'
   panelComoLlegar.style.display = 'flex'
--- pages/mapa.php
+++ pages/mapa.php
@@ -2,72 +2,96 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/mapa.css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css" />
-    <script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/css/mapa.css">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+    integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css" />
+  <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css" />
+  <script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
 </head>
 
 <body>
-    <div>
-        <button onclick="showLineas()">Lineas</button>
-        <button onclick="showParadas()">Paradas</button>
-        <button onclick="showBicis()">Bicicletas</button>
-        <button onclick="showComoLlegar()">Cómo llegar</button>
-    </div>
+  <aside class="aside-1">
+    <ul>
+      <li class="aside-icon">
+        <button onclick="showLineas()">
+          <img src="https://www.jobalign.com/wp-content/uploads/2017/07/Icon-Placeholder-1.png">
+          <p>Lineas</p>
+        </button>
+      </li>
+      <li class="aside-icon">
+        <button onclick="showParadas()">
+          <img src="https://www.jobalign.com/wp-content/uploads/2017/07/Icon-Placeholder-1.png">
+          <p>Paradas</p>
+        </button>
+      </li>
+      <li class="aside-icon">
+        <button onclick="showBicis()">
+          <img src="https://www.jobalign.com/wp-content/uploads/2017/07/Icon-Placeholder-1.png">
+          <p>Bicicletas</p>
+        </button>
+      </li>
+      <li class="aside-icon">
+        <button onclick="showComoLlegar()">
+          <img src="https://www.jobalign.com/wp-content/uploads/2017/07/Icon-Placeholder-1.png">
+          <p>Cómo llegar</p>
+        </button>
+      </li>
+    </ul>
+  </aside>
+  <aside class="aside-2">
     <div id="panelLinea">
-        <select id="selectLinea" onchange="setLinea(this.value)">
-            <option value="">Selecciona una linea</option>
-            <?php
-            include_once "../util/executeQuery.php";
-            $query = "SELECT * FROM LINEAS";
-            $resultado = executeQuery($query);
-            while ($fila = mysqli_fetch_assoc($resultado)) {
-                echo "<option value='" . $fila['line'] . "'>" . $fila['label'] . ": " . $fila['nameA'] . " - " . $fila['nameB'] . "</option>";
-            }
-            ?>
-        </select>
-        <button onclick="setLinea(null,1)">A -> B</button>
-        <button onclick="setLinea(null,2)">B -> A</button>
+      <select id="selectLinea" onchange="setLinea(this.value)">
+        <option value="">Selecciona una linea</option>
+        <?php
+        include_once "../util/executeQuery.php";
+        $query = "SELECT * FROM LINEAS";
+        $resultado = executeQuery($query);
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+          echo "<option value='" . $fila['line'] . "'>" . $fila['label'] . ": " . $fila['nameA'] . " - " . $fila['nameB'] . "</option>";
+        }
+        ?>
+      </select>
+      <button onclick="setLinea(null,1)">A -> B</button>
+      <button onclick="setLinea(null,2)">B -> A</button>
     </div>
     <div id="panelParadas">
-        <form id="searchParadas" onsubmit="searchParadas(event)">
-            <input type="text" name="place" placeholder="Buscar paradas cercanas a zona o calle">
-            <input type="number" name="number" placeholder="Nº calle (opcional)">
-            <label for="per">Perímetro:</label>
-            <input id="per" type="range" name="per" min="100" max="1000" value="500" step="50"
-                onchange="updateRange(event)">
-            <p id="per-value">500</p>
-            <button type="submit">Buscar</button>
-        </form>
-        <br>
-        <button onclick="setAllParadas()">Mostrar todas las paradas</button>
-        <button onclick="setParadaCursor()">Mostrar paradas a un punto</button>
+      <form id="searchParadas" onsubmit="searchParadas(event)">
+        <input type="text" name="place" placeholder="Buscar paradas cercanas a zona o calle">
+        <input type="number" name="number" placeholder="Nº calle (opcional)">
+        <label for="per">Perímetro:</label>
+        <input id="per" type="range" name="per" min="100" max="1000" value="500" step="50"
+          onchange="updateRange(event)">
+        <p id="per-value">500</p>
+        <button type="submit">Buscar</button>
+      </form>
+      <br>
+      <button onclick="setAllParadas()">Mostrar todas las paradas</button>
+      <button onclick="setParadaCursor()">Mostrar paradas a un punto</button>
     </div>
     <div id="panelComoLlegar">
-        <input type="text" name="place" placeholder="Elige un punto de partida en el mapa">
+      <input type="text" name="place" placeholder="Elige un punto de partida en el mapa">
     </div>
-    <div id="map" style="width: 100%; height: 500px;"></div>
     <div id="info">
-        <h2 id='nombre'></h2>
-        <div id="detalles"></div>
+      <h2 id='nombre'></h2>
+      <div id="detalles"></div>
     </div>
-    <script src="/js/api.js"></script>
-    <script src="/js/mapa.js"></script>
-    <script src="/js/mapaLineas.js"></script>
-    <script src="/js/mapaParadas.js"></script>
-    <script src="/js/mapaInfo.js"></script>
-    <script src="/js/mapaBicis.js"></script>
-    <script src="/js/mapaCL.js"></script>
-    <script src="/js/mapaPanel.js"></script>
+  </aside>
+  <div id="map" style="width: 100%; height: 100vh;"></div>
+  <script src="/js/api.js"></script>
+  <script src="/js/mapa.js"></script>
+  <script src="/js/mapaLineas.js"></script>
+  <script src="/js/mapaParadas.js"></script>
+  <script src="/js/mapaInfo.js"></script>
+  <script src="/js/mapaBicis.js"></script>
+  <script src="/js/mapaCL.js"></script>
+  <script src="/js/mapaPanel.js"></script>
 </body>
 
 </html>
